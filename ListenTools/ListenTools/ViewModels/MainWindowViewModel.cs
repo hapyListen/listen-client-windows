63f00ce55ab0cc77af8717cd8b451504338c1f44
南笺 <1587398183@qq.com>
2024-07-19 23:10:07 +0800

    添加遗漏文件

---
+++ ListenTools/ListenTools/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ListenTools.ViewModels;
+
+public partial class MainWindowViewModel : Base.ViewModelBase
+{
+    
+#pragma warning disable CA1822 // Mark members as static
+    public string Greeting => "Welcome to Avalonia!";
+#pragma warning restore CA1822 // Mark members as static
+    
+}
