305a365ed61d64e3f3d9a7b72ad0a9fa9df8d61f
南笺 <1587398183@qq.com>
2024-07-28 23:44:27 +0800

    【新增】完善主界面

---
+++ ListenTools/ListenTools/Models/MainMenuItemBdm.cs
@@ -0,0 +1,10 @@
+using Avalonia.Media;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ListenTools.Models;
+
+public class MainMenuItemBdm 
+{
+       public Geometry Icon { get; set; }
+       public string Title { get; set; }
+}
--- ListenTools/ListenTools/ViewModels/MainWindowViewModel.cs
+++ ListenTools/ListenTools/ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,51 @@
-﻿namespace ListenTools.ViewModels;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Avalonia.Controls;
+using Avalonia.Media;
+using CommunityToolkit.Mvvm.ComponentModel;
+using ListenTools.Models;
+
+namespace ListenTools.ViewModels;
 
 public partial class MainWindowViewModel : Base.ViewModelBase
 {
+
+    #region Properties
+
+    /// <summary>
+    /// 主菜单
+    /// </summary>
+    public IList<Models.MainMenuItemBdm> MainMenuSource { get; } = new ObservableCollection<MainMenuItemBdm>();
+
+    #endregion
     
-    public string Greeting => "Welcome to Avalonia!";
-
-
     public MainWindowViewModel()
     {
         Media.Instance.TcpClientTest();
+        
+        MainMenuSource.Add(new ()
+        {
+            Title = "主页",
+            Icon = App.Current.FindResource("HomeIcon") as Geometry
+        });
+        MainMenuSource.Add(new ()
+        {
+            Title = "音乐",
+            Icon = App.Current.FindResource("MusicIcon") as Geometry
+        });
+        MainMenuSource.Add(new ()
+        {
+            Title = "聊天室",
+            Icon = App.Current.FindResource("ChatRoomIcon") as Geometry
+        });
+        
+        MainMenuSource.Add(new ()
+        {
+            Title = "直播间",
+            Icon = App.Current.FindResource("LiveHomeIcon") as Geometry
+        }); 
+      
     }
+    
 }
