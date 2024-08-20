26db6ee374bb8e4211bcef641003cf44221b32d4
miye <1587398183@qq.com>
2024-08-20 23:04:36 +0800

    【修改】调整房间设置界面

---
+++ ListenTools/ListenTools/Views/ChatRoomPages/ChatRoomSettingView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ListenTools.Views.ChatRoomPages;
+
+public partial class ChatRoomSettingView : UserControl
+{
+    public ChatRoomSettingView()
+    {
+        InitializeComponent();
+    }
+}
--- ListenTools/ListenTools/Views/ChatRoomPages/ChatRoomView.axaml.cs
+++ ListenTools/ListenTools/Views/ChatRoomPages/ChatRoomView.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 using Avalonia.Threading;
 using CommunityToolkit.Mvvm.Messaging;
 using ListenTools.Global;
@@ -27,8 +28,26 @@ public partial class ChatRoomView : UserControl
         {
             Dispatcher.UIThread.Post(delegate
             {
-                ChatRoomEditDialog dialog = new();
-                dialog.ShowDialog(MainWindow.GlobalMainWindow);
+                // ChatRoomEditDialog dialog = new();
+                // dialog.ShowDialog(MainWindow.GlobalMainWindow);
+                var rootGrid = this.FindControl<Grid>("PART_RoomParentGrid");
+                var settingMarkPanel = this.FindControl<Panel>("SettingMarkPanel");
+                var settingControl = this.FindControl<ChatRoomSettingView>("PART_RoomSettingControl");
+                if (rootGrid == null)
+                    return;
+                if (settingMarkPanel == null)
+                    return;
+                if (settingControl == null)
+                    return;
+                var rootGridScale = rootGrid.RenderTransform as ScaleTransform;
+                rootGridScale.ScaleX = 0.95;
+                rootGridScale.ScaleY = 0.95;
+                
+                // rootGrid.RenderTransform = new ScaleTransform(0.95, 0.95);
+                settingMarkPanel.IsVisible = true;
+                // settingControl.RenderTransform = new TranslateTransform(0, 0);
+                var settingControlTransform = settingControl.RenderTransform as TranslateTransform;
+                settingControlTransform.X = 0;
             });
         });
     }
